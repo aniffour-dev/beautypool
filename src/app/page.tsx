661d0f71786ea0b43667d06c5d2e765a3deea0e5
Hamza Aniffour <aniffourmersh@gmail.com>
2024-12-09 22:19:21 +0100

    HomePage Section Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Hero from '@/components/static/Hero'
 import React from 'react'
 // import Hero from '@/app/components/static/sections/Hero'
 import Footer from "@/components/global/footer/Footer";
-// import Achievement from './components/static/sections/Achievement';
-// import WhyUs from './components/static/sections/WhyUs';
-// import Tranding from './components/static/sections/Tranding';
+import Achievement from '@/components/static/sections/Achievement';
+import WhyUs from '@/components/static/sections/WhyUs';
+import Tranding from '@/components/static/sections/Trending';
 import NewBeauty from '@/components/static/sections/NewBeauty';
-// import Reviews from './components/static/sections/Reviews';
+import Reviews from '@/components/static/sections/Reviews';
 
 
 const Home = () => {
@@ -15,10 +15,10 @@ const Home = () => {
     <>
       <Hero />
       <NewBeauty /> 
-      {/* <Tranding />
+      <Tranding />
       <Achievement />
-      <Reviews /> */}
-      {/* <WhyUs /> */}
+      <Reviews />
+      <WhyUs />
       <Footer />
     </>
   )
