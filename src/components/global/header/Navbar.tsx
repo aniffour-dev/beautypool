c56f6562456897bdc4857f39dbe7e0dfc7156bb7
Hamza Aniffour <aniffourmersh@gmail.com>
2024-12-09 20:59:43 +0100

    Navbar Added

---
+++ src/components/global/header/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "./Logo";
+import RightBar from "./RightBar";
+
+const Navbar = () => {
+  return (
+    <div className="relative z-50">
+      <div className="flex justify-between items-center max-w-5xl mx-auto pt-10 px-5 lg:px-0">
+        <Logo />
+        <RightBar />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
