64dacce6d4960a1c1535ab0a886d01188f590c0d
Hamza Aniffour <aniffourmersh@gmail.com>
2024-12-09 22:05:07 +0100

    New Beauty Section Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 import Hero from '@/components/static/Hero'
 import React from 'react'
 // import Hero from '@/app/components/static/sections/Hero'
-// import Footer from "@/app/components/global/footer/Footer";
+import Footer from "@/components/global/footer/Footer";
 // import Achievement from './components/static/sections/Achievement';
 // import WhyUs from './components/static/sections/WhyUs';
 // import Tranding from './components/static/sections/Tranding';
-// import NewBeauty from './components/static/sections/NewBeauty';
+import NewBeauty from '@/components/static/sections/NewBeauty';
 // import Reviews from './components/static/sections/Reviews';
 
 
@@ -14,12 +14,12 @@ const Home = () => {
   return (
     <>
       <Hero />
-      {/* <NewBeauty /> 
-      <Tranding />
+      <NewBeauty /> 
+      {/* <Tranding />
       <Achievement />
-      <Reviews />
-      <WhyUs />
-      <Footer /> */}
+      <Reviews /> */}
+      {/* <WhyUs /> */}
+      <Footer />
     </>
   )
 }
