cb5432fdd3b6b226b5efbf5ad0319106ef33717e
Hamza Aniffour <aniffourmersh@gmail.com>
2024-12-10 10:20:18 +0100

    next config backend url added

--- src/components/global/header/Navbar.tsx
+++ src/components/global/header/Navbar.tsx
@@ -6,6 +6,7 @@ const Navbar = () => {
   return (
     <div className="relative z-50">
       <div className="flex justify-between items-center max-w-5xl mx-auto pt-10 px-5 lg:px-0">
+        <div className="block lg:hidden"></div>
         <Logo />
         <RightBar />
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,48 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        first: "moveVertical 50s ease infinite",
+        second: "moveInCircle 100s reverse infinite",
+        third: "moveInCircle 60s linear infinite",
+        fourth: "moveHorizontal 70s ease infinite",
+        fifth: "moveInCircle 40s ease infinite",
+      },
+      keyframes: {
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+      },
     },
   },
   plugins: [],
