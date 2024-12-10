5236a8e27f8430f678ef087883d5105f342c65bf
idriss-bk <drissboukdirbk@gmail.com>
2024-12-10 16:31:59 +0400

    update bg

--- src/components/global/footer/Footer.tsx
+++ src/components/global/footer/Footer.tsx
@@ -17,29 +17,29 @@ export default function Footer() {
           <Link href="/">
             {/* Responsive Logo */}
             <Image
-              src="/assets/logo.svg"
+              src="/assets/logo.webp"
               alt="logo"
               width={120}
               height={40}
               className="object-contain sm:w-[90px] md:w-[120px] lg:w-[140px]"
             />
           </Link>
-          <div className="flex gap-4 text-black mt-2">
+          <div className="flex gap-4 text-[#ee8b7a] mt-2">
             {/* Social Media Icons */}
             <FaFacebookF
-              className="text-xl cursor-pointer hover:text-[#f4b8ae] transition-all duration-200"
+              className="text-xl cursor-pointer hover:text-[#de7664] transition-all duration-200"
               aria-label="Facebook"
             />
             <FaTwitter
-              className="text-xl cursor-pointer hover:text-[#f4b8ae] transition-all duration-200"
+              className="text-xl cursor-pointer hover:text-[#de7664] transition-all duration-200"
               aria-label="Twitter"
             />
             <FaLinkedinIn
-              className="text-xl cursor-pointer hover:text-[#f4b8ae] transition-all duration-200"
+              className="text-xl cursor-pointer hover:text-[#de7664] transition-all duration-200"
               aria-label="LinkedIn"
             />
             <FaInstagram
-              className="text-xl cursor-pointer hover:text-[#f4b8ae] transition-all duration-200"
+              className="text-xl cursor-pointer hover:text-[#de7664] transition-all duration-200"
               aria-label="Instagram"
             />
           </div>
